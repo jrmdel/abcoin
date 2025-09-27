c60a7bdf447c9d686268877d52113d33dfc934c7
jrmdel <jeremie.deletraz@gmail.com>
2025-09-28 00:22:29 +0200

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { NotificationService } from 'src/notification/notification.service';
 
 async function bootstrap(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
   app.setGlobalPrefix('api');
   app.enableVersioning({
@@ -13,7 +13,20 @@ async function bootstrap(): Promise<INestApplication> {
   });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  setupSwagger(app);
+
+  await app.listen(process.env.PORT ?? 3000);
   return app;
+}
+
+function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Alertin Bot Coin')
+    .setDescription('The ABCoin API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = (): OpenAPIObject => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
 }
 
 bootstrap()
